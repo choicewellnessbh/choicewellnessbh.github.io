5bc56ae7efdddf98b22800f21dfd5508cbc91fb5
choicewellnessbh <ctMGAFiwFS2kdsb>
2021-06-21 23:43:35 -0500

    Changed email

--- mail.php
+++ mail.php
@@ -3,7 +3,7 @@
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         # FIX: Replace this email with recipient email
-        $mail_to = "choicewellnessbis@gmail.com";
+        $mail_to = "choicebusinessinfo@gmail.com";
         
         # Sender Data
         $subject = trim($_POST["subject"]);
